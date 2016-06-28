e5644808e7d49c205de9b2a1df20bfab411c58b2
catena <catena@protonmail.com>
2016-06-28 16:56:33 +0300

    update to latest code version

--- src/test/scala/scorex/ScorexTestSuite.scala
+++ src/test/scala/scorex/ScorexTestSuite.scala
@@ -1,12 +1,10 @@
 package scorex
 
 import org.scalatest.Suites
-import scorex.account.AccountSpecification
 import scorex.crypto.SigningFunctionsSpecification
 import scorex.network.HandshakeSpecification
 
 class ScorexTestSuite extends Suites(
-  new AccountSpecification,
   new SigningFunctionsSpecification,
   new HandshakeSpecification
 )
