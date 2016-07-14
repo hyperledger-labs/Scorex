51e4e537b5877ca80dd9661cff29d77113d8dc72
kushti <alex.chepurnoy@iohk.io>
2016-07-14 14:56:06 +0300

    pool-related functions pulled out from TransactionalModule into UnconfirmedTransactionsDatabase

--- src/main/scala/scorex/transaction/TransactionalModule.scala
+++ src/main/scala/scorex/transaction/TransactionalModule.scala
@@ -10,7 +10,7 @@ import scorex.transaction.wallet.Wallet
 
 
 trait TransactionalModule[P <: Proposition, TX <: Transaction[P, TX], TData <: TransactionalData[TX]]
-  extends UnconfirmedTransactionsDatabase[TX] with MinimalState[P, TX] with ScorexLogging {
+  extends UnconfirmedTransactionsDatabase[TX, TData] with MinimalState[P, TX] with ScorexLogging {
 
   type SH <: SecretHolder[P with AddressableProposition, _ <: Proof[P]]
   type W <: Wallet[_ <: P, _ <: TransactionalModule[P, TX, TData]]
@@ -27,12 +27,6 @@ trait TransactionalModule[P <: Proposition, TX <: Transaction[P, TX], TData <: T
   def totalFee(blockData: TData): Long = transactions(blockData).map(_.fee).sum
 
   def stop(): Unit = wallet.close()
-
-  def packUnconfirmed(): TData
-
-  def clearFromUnconfirmed(data: TData): Unit
-
-  def onNewOffchainTransaction(transaction: TX): Unit
 
   val genesisData: TData
 }
--- src/main/scala/scorex/transaction/UnconfirmedTransactionsDatabase.scala
+++ src/main/scala/scorex/transaction/UnconfirmedTransactionsDatabase.scala
@@ -1,12 +1,20 @@
 package scorex.transaction
 
-trait UnconfirmedTransactionsDatabase[TX <: Transaction[_, TX]] {
+import scorex.block.TransactionalData
+
+trait UnconfirmedTransactionsDatabase[TX <: Transaction[_, TX], TData <: TransactionalData[TX]] {
 
   def putIfNew(tx: TX): Boolean
 
   def all(): Seq[TX]
 
-  def getBySignature(signature: Array[Byte]): Option[TX]
+  def getById(id: Array[Byte]): Option[TX]
+
+  def packUnconfirmed(): TData
+
+  def clearFromUnconfirmed(data: TData): Unit
+
+  def onNewOffchainTransaction(transaction: TX): Unit
 
   def remove(tx: TX)
 }
