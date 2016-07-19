5f72f99cd517bc5c76b624327e66a5a111476a9d
_ <catena@protonmail.com>
2016-07-19 15:20:48 +0300

    leave json and bytes abstract

--- src/main/scala/scorex/block/ConsensusData.scala
+++ src/main/scala/scorex/block/ConsensusData.scala
@@ -1,18 +1,15 @@
 package scorex.block
 
-import io.circe.Json
 import scorex.serialization.{BytesSerializable, JsonSerializable}
 
 trait ConsensusData extends BytesSerializable with JsonSerializable {
+
   import ConsensusData.BlockId
 
   val BlockIdLength: Int
 
   val parentId: BlockId
 
-  override val bytes: Array[Byte] = ???
-
-  override val json: Json = ???
 }
 
 object ConsensusData {
