f7d7ead627f569f0fa3ec8e68ec1aa73a386a3cb
kushti <alex.chepurnoy@iohk.io>
2016-07-28 00:18:51 +0300

    history setting removed

--- src/main/scala/scorex/consensus/ConsensusSettings.scala
+++ src/main/scala/scorex/consensus/ConsensusSettings.scala
@@ -5,9 +5,6 @@ import io.circe.Json
 trait ConsensusSettings {
   val settingsJSON: Map[String, Json]
 
-  private val DefaultHistory = "blockchain"
-  lazy val history = settingsJSON.get("history").flatMap(_.asString).getOrElse(DefaultHistory)
-
   private val DefaultMaxRollback = 100
   lazy val MaxRollback = settingsJSON.get("max-rollback").flatMap(_.asNumber).flatMap(_.toInt).getOrElse(DefaultMaxRollback)
 }
