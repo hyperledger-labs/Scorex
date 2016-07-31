0f97374393a713e6fae81f0b0a4eb372e33837ac
catena <catena@protonmail.com>
2016-07-31 14:38:57 +0000

    Helper functions to serialize/parse bytes

--- src/main/scala/scorex/serialization/BytesParseable.scala
+++ src/main/scala/scorex/serialization/BytesParseable.scala
@@ -1,5 +1,8 @@
 package scorex.serialization
 
+import com.google.common.primitives.Ints
+
+import scala.annotation.tailrec
 import scala.util.Try
 
 /**
@@ -7,4 +10,16 @@ import scala.util.Try
   */
 trait BytesParseable[T] {
   def parseBytes(bytes: Array[Byte]): Try[T]
+
+  protected def parseArraySizes(bytes: Array[Byte]): Seq[Array[Byte]] = {
+    @tailrec
+    def loop(b: Array[Byte], acc: Seq[Array[Byte]]): Seq[Array[Byte]] = {
+      val length = Ints.fromByteArray(b.take(4))
+      val objBytes = b.slice(4, 4 + length)
+      val restBytes = b.slice(4 + length, b.length)
+      if (restBytes.isEmpty) objBytes +: acc
+      else loop(restBytes, objBytes +: acc)
+    }
+    loop(bytes, Seq())
+  }
 }
