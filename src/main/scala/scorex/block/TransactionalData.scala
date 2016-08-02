d3e053219eff0387aa9c82e41984cb0e45713c1a
kushti <alex.chepurnoy@iohk.io>
2016-08-02 12:45:08 +0300

    TransactionalData comment

--- src/main/scala/scorex/block/TransactionalData.scala
+++ src/main/scala/scorex/block/TransactionalData.scala
@@ -3,9 +3,16 @@ package scorex.block
 import scorex.serialization.{BytesSerializable, JsonSerializable}
 import scorex.transaction.Transaction
 
+/**
+  * Part of block which contains transaction as well as some metadata
+  * about them (e.g. Merkle tree root value)
+  * If this is a part of blockheader, not a full block, mbTransactions are about None
+  * and thus headerOnly is true
+  *
+  * @tparam TX - base type of transactions to be packed into a block
+  */
 trait TransactionalData[TX <: Transaction[_, TX]] extends BytesSerializable with JsonSerializable {
   val mbTransactions: Option[Traversable[TX]]
-
   lazy val headerOnly = mbTransactions.isDefined
 }
 
