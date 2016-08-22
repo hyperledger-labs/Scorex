3bbfe80842d5fbfac9f9d893ea9f28e6786662d8
kushti <alex.chepurnoy@iohk.io>
2016-08-22 16:31:43 +0300

    DefaultWallet25519 partly implemented

--- src/main/scala/scorex/transaction/wallet/WalletOld.scala
+++ src/main/scala/scorex/transaction/wallet/WalletOld.scala
@@ -4,16 +4,19 @@ import java.io.File
 
 import com.google.common.primitives.{Bytes, Ints}
 import org.h2.mvstore.{MVMap, MVStore}
+import org.mapdb.{DBMaker, HTreeMap}
+import org.mapdb.serializer.SerializerByteArray
 import scorex.crypto.encode.Base58
 import scorex.crypto.hash.SecureCryptographicHash
 import scorex.settings.Settings
 import scorex.transaction.Transaction
 import scorex.transaction.box.Box
 import scorex.transaction.box.proposition.{Proposition, PublicImage, PublicKey25519Proposition}
-import scorex.transaction.state.{PrivateKey25519, PublicKey25519, Secret}
+import scorex.transaction.state.{PrivateKey25519, PrivateKey25519Companion, PublicKey25519, Secret}
 import scorex.utils.{ScorexLogging, randomBytes}
 
 import scala.collection.concurrent.TrieMap
+import scala.collection.JavaConversions._
 
 case class WalletBox[P <: Proposition](box: Box[P], transactionId: Array[Byte], createdAt: Int, destroyedAt: Option[Int])
 
@@ -38,20 +41,74 @@ trait Wallet[P <: Proposition, TX <: Transaction[P, TX], S <: Secret, PI <: Publ
 
   def historyBoxes: Seq[WalletBox[P]]
 
-  def publicKeys: Seq[PI]
+  def publicKeys: Set[PI]
 
   //todo: protection?
-  def secrets: Seq[S]
+  def secrets: Set[S]
 
-  def secretByPublicImage(publicImage: PI): S
+  def secretByPublicImage(publicImage: PI): Option[S]
 }
 
 
 //todo: HKDF
-class DefaultWallet25519[TX <: Transaction[PublicKey25519Proposition, TX] ]
+// todo: encryption
+case class DefaultWallet25519[TX <: Transaction[PublicKey25519Proposition, TX]]
+(settings: Settings)
   extends Wallet[PublicKey25519Proposition, TX, PrivateKey25519, PublicKey25519] {
 
-  override def generateNewSecret(): Wallet[PublicKey25519Proposition, TX, PrivateKey25519, PublicKey25519] = ???
+  val walletFileOpt: Option[File] = settings.walletDirOpt.map(walletDir => new java.io.File(walletDir, "wallet.s.dat"))
+  val password: String = settings.walletPassword
+  val seedOpt: Option[Array[Byte]] = settings.walletSeed
+
+  val db = DBMaker
+    .fileDB("wallet.dat")
+    .make()
+
+  private val dbSeed = db.atomicString("seed").createOrOpen()
+
+  lazy val seed: Array[Byte] = Option(dbSeed.get())
+    .map(Base58.decode)
+    .flatMap(_.toOption)
+    .getOrElse {
+      val s = seedOpt.getOrElse {
+        val Attempts = 10
+        val SeedSize = 64
+        lazy val randomSeed = randomBytes(SeedSize)
+        def readSeed(limit: Int = Attempts): Array[Byte] = {
+          println("Please type your wallet seed or type Enter to generate random one")
+          val typed = scala.io.StdIn.readLine()
+          if (typed.isEmpty) {
+            println(s"Your random generated seed is ${Base58.encode(randomSeed)}")
+            randomSeed
+          } else
+            Base58.decode(typed).getOrElse {
+              if (limit > 0) {
+                println("Wallet seed should be correct Base58 encoded string.")
+                readSeed(limit - 1)
+              } else throw new Error("Sorry you have made too many incorrect seed guesses")
+            }
+        }
+        readSeed()
+      }
+      dbSeed.set(Base58.encode(s))
+      s
+    }
+
+  private def lastNonce = db.atomicInteger("nonce").createOrOpen()
+
+  private lazy val dbSecrets: HTreeMap[Array[Byte], Array[Byte]] =
+    db.hashMap("secrets", new SerializerByteArray, new SerializerByteArray).createOrOpen()
+
+  override def generateNewSecret(): Wallet[PublicKey25519Proposition, TX, PrivateKey25519, PublicKey25519] = {
+    val nonce = lastNonce.incrementAndGet()
+    val randomSeed = SecureCryptographicHash(Bytes.concat(Ints.toByteArray(nonce), seed))
+    val (priv, pub) = PrivateKey25519Companion.generateKeys(randomSeed)
+
+    dbSecrets.put(pub.publicKeyBytes, priv.privKeyBytes)
+    db.commit()
+    db.close()
+    new DefaultWallet25519[TX](settings)
+  }
 
   override def scan(tx: Transaction[PublicKey25519Proposition, _ <: Transaction[PublicKey25519Proposition, _]]): Wallet[PublicKey25519Proposition, TX, PrivateKey25519, PublicKey25519] = ???
 
@@ -59,18 +116,17 @@ class DefaultWallet25519[TX <: Transaction[PublicKey25519Proposition, TX] ]
 
   override def historyBoxes: Seq[WalletBox[PublicKey25519Proposition]] = ???
 
-  override def publicKeys: Seq[PublicKey25519] = ???
+  override def publicKeys: Set[PublicKey25519] =
+    dbSecrets.getKeys.map(PublicKey25519.apply).toSet
 
   //todo: protection?
-  override def secrets: Seq[PrivateKey25519] = ???
+  override def secrets: Set[PrivateKey25519] =
+  dbSecrets.getEntries.map(e => PrivateKey25519(e.getValue, e.getKey)).toSet
 
-  override def secretByPublicImage(publicImage: PublicKey25519): PrivateKey25519 = ???
+  override def secretByPublicImage(publicImage: PublicKey25519): Option[PrivateKey25519] =
+    Option(dbSecrets.get(publicImage))
+      .map(privBytes => PrivateKey25519(privBytes, publicImage.publicKeyBytes))
 }
-
-
-
-
-
 
 
 //todo: add txs watching
@@ -96,32 +152,6 @@ class WalletOld[P <: Proposition, S <: Secret](settings: Settings)
   private val secretsPersistence: MVMap[Int, Array[Byte]] = database.openMap("secretHolders")
   private val seedPersistence: MVMap[String, Array[Byte]] = database.openMap("seed")
   private val noncePersistence: MVMap[String, Int] = database.openMap("nonce")
-
-  if (Option(seedPersistence.get("seed")).isEmpty) {
-    val seed = seedOpt.getOrElse {
-      val Attempts = 10
-      val SeedSize = 64
-      lazy val randomSeed = randomBytes(SeedSize)
-      def readSeed(limit: Int = Attempts): Array[Byte] = {
-        println("Please type your wallet seed or type Enter to generate random one")
-        val typed = scala.io.StdIn.readLine()
-        if (typed.isEmpty) {
-          println(s"Your random generated seed is ${Base58.encode(randomSeed)}")
-          randomSeed
-        } else
-          Base58.decode(typed).getOrElse {
-            if (limit > 0) {
-              println("Wallet seed should be correct Base58 encoded string.")
-              readSeed(limit - 1)
-            } else throw new Error("Sorry you have made too many incorrect seed guesses")
-          }
-      }
-      readSeed()
-    }
-    seedPersistence.put("seed", seed)
-  }
-
-  private lazy val seed: Array[Byte] = seedPersistence.get("seed")
 
   private val secretsCache: TrieMap[String, S] = ???
   /*{
