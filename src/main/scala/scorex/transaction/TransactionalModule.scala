7ddffdc6ccfab2c6c4b3c92a9bae0b1db26bef07
kushti <alex.chepurnoy@iohk.io>
2016-08-22 17:20:08 +0300

    transactional module made pure

--- src/main/scala/scorex/transaction/TransactionalModule.scala
+++ src/main/scala/scorex/transaction/TransactionalModule.scala
@@ -10,15 +10,7 @@ import scorex.utils.ScorexLogging
 trait TransactionalModule[P <: Proposition, TX <: Transaction[P, TX], TData <: TransactionalData[TX]]
   extends ScorexLogging {
 
-  //type SH <: SecretHolder[P with AddressableProposition, _ <: Proof[P]]
-
-  val mempool: MemoryPool[TX]
-  val state: MinimalState[P, TX]
-
   val settings: Settings
-
-  //val generator: SecretHolderGenerator[SH]
-  //val wallet: W
 
   def transactions(blockData: TData): Seq[TX]
 
