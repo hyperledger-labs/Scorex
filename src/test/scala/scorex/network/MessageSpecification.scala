118f1aeae3ed3149edbd151354c7f18171956c17
catena <catena@protonmail.com>
2016-09-15 13:05:34 +0300

    Test RequestModifierSpec close #3

--- src/test/scala/scorex/network/MessageSpecification.scala
+++ src/test/scala/scorex/network/MessageSpecification.scala
@@ -5,7 +5,7 @@ import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 import org.scalatest.{Matchers, PropSpec}
 import scorex.ObjectGenerators
 import scorex.core.network.message.BasicMsgDataTypes.InvData
-import scorex.core.network.message.InvSpec
+import scorex.core.network.message.{RequestModifierSpec, InvSpec}
 import scorex.core.transaction.NodeViewModifier._
 
 import scala.util.Try
@@ -27,11 +27,30 @@ with ObjectGenerators {
     }
   }
 
-  property("InvData for big arrays") {
+  property("InvData should not serialize big arrays") {
     forAll(Arbitrary.arbitrary[Byte], Gen.listOfN(InvSpec.MaxObjects + 1, modifierIdGen)) { (b: Byte, s: Seq[ModifierId]) =>
       val data: InvData = (b, s)
       Try(InvSpec.serializeData(data)).isSuccess shouldBe false
     }
   }
+
+  property("RequestModifierSpec serialization/deserialization") {
+    forAll(invDataGen) { data: InvData =>
+      whenever(data._2.length < InvSpec.MaxObjects) {
+        val bytes = RequestModifierSpec.serializeData(data)
+        val recovered = InvSpec.deserializeData(bytes).get
+        val bytes2 = RequestModifierSpec.serializeData(recovered)
+        bytes shouldEqual bytes2
+      }
+    }
+  }
+
+  property("RequestModifierSpec should not serialize big arrays") {
+    forAll(Arbitrary.arbitrary[Byte], Gen.listOfN(InvSpec.MaxObjects + 1, modifierIdGen)) { (b: Byte, s: Seq[ModifierId]) =>
+      val data: InvData = (b, s)
+      Try(InvSpec.serializeData(data)).isSuccess shouldBe false
+    }
+  }
+
 
 }
