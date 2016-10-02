1139c49dcbfbc357b389445f1b1fd148bd44d8e3
catena <catena@protonmail.com>
2016-10-02 17:49:06 +0300

    json for simple block

--- examples/src/main/scala/examples/curvepos/transaction/SimpleBlock.scala
+++ examples/src/main/scala/examples/curvepos/transaction/SimpleBlock.scala
@@ -1,17 +1,19 @@
 package examples.curvepos.transaction
 
+import com.google.common.primitives.{Ints, Longs}
+import examples.curvepos.transaction.SimpleBlock._
 import io.circe.Json
+import io.circe.syntax._
 import scorex.core.block.Block
+import scorex.core.block.Block._
 import scorex.core.crypto.hash.FastCryptographicHash
 import scorex.core.transaction.NodeViewModifier.ModifierTypeId
-import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 import scorex.core.transaction.NodeViewModifierCompanion
+import scorex.core.transaction.box.proposition.PublicKey25519Proposition
+import scorex.crypto.encode.Base58
 import shapeless.{::, HNil}
 
 import scala.util.Try
-import Block._
-import SimpleBlock._
-import com.google.common.primitives.{Ints, Longs}
 
 case class SimpleBlock(override val parentId: BlockId,
                        override val timestamp: Long,
@@ -35,7 +37,14 @@ case class SimpleBlock(override val parentId: BlockId,
 
   override val version: Version = 0: Byte
 
-  override def json: Json = ???
+  override def json: Json = Map(
+    "parentId" -> Base58.encode(parentId).asJson,
+    "timestamp" -> timestamp.asJson,
+    "generationSignature" -> Base58.encode(generationSignature).asJson,
+    "baseTarget" -> baseTarget.asJson,
+    "generator" -> Base58.encode(generator.pubKeyBytes).asJson,
+    "txs" -> txs.map(_.json).asJson
+  ).asJson
 
   override lazy val blockFields: BlockFields = parentId :: timestamp :: version :: generationSignature :: baseTarget :: generator :: txs :: HNil
 }
@@ -54,12 +63,12 @@ object SimpleBlockCompanion extends NodeViewModifierCompanion[SimpleBlock] {
       block.generationSignature ++
       Longs.toByteArray(block.baseTarget) ++
       block.generator.pubKeyBytes ++ {
-        val cntBytes = Ints.toByteArray(block.txs.size)
-        block.txs.foldLeft(cntBytes){case (bytes, tx) =>
-          val txBytes = tx.companion.bytes(tx)
-          bytes ++ Ints.toByteArray(txBytes.size) ++ txBytes
-        }
+      val cntBytes = Ints.toByteArray(block.txs.size)
+      block.txs.foldLeft(cntBytes) { case (bytes, tx) =>
+        val txBytes = tx.companion.bytes(tx)
+        bytes ++ Ints.toByteArray(txBytes.size) ++ txBytes
       }
+    }
 
   }
 
