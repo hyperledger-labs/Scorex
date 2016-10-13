ef06f300069ba641b71e189c0587fa796b42b628
kushti <alex.chepurnoy@iohk.io>
2016-10-13 13:33:58 +0300

    SimpleNodeViewHolder: definitions rearrangement, modifierCompanions = ???

--- examples/src/main/scala/examples/curvepos/SimpleNodeViewHolder.scala
+++ examples/src/main/scala/examples/curvepos/SimpleNodeViewHolder.scala
@@ -7,14 +7,14 @@ import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 
 class SimpleNodeViewHolder extends NodeViewHolder[PublicKey25519Proposition, SimpleTransaction, SimpleBlock] {
   override type HIS = SimpleBlockchain
+  override type MS = SimpleState
+  override type VL = SimpleWallet
+  override type MP = SimpleMemPool
+
+  override val modifierCompanions: Map[ModifierTypeId, NodeViewModifierCompanion[_ <: NodeViewModifier]] = ???
 
   override def restoreState(): Option[(HIS, MS, VL, MP)] = None
 
   override protected def genesisState: (HIS, MS, VL, MP) =
     (new SimpleBlockchain, new SimpleState, SimpleWallet(), new SimpleMemPool)
-
-  override type MS = SimpleState
-  override type VL = SimpleWallet
-  override type MP = SimpleMemPool
-  override val modifierCompanions: Map[ModifierTypeId, NodeViewModifierCompanion[_ <: NodeViewModifier]] = null
 }
