9817b7373d1c0806ecc8aaec291ba5f0359ab28d
kushti <alex.chepurnoy@iohk.io>
2016-10-14 18:22:47 +0300

    LocalInterface more or less ready

--- src/main/scala/scorex/core/LocalInterface.scala
+++ src/main/scala/scorex/core/LocalInterface.scala
@@ -2,11 +2,16 @@ package scorex.core
 
 import akka.actor.{Actor, ActorRef}
 import scorex.core.NodeViewHolder._
+import scorex.core.transaction.Transaction
+import scorex.core.transaction.box.proposition.Proposition
 
 /**
   *
   */
-class LocalInterface(viewHolderRef: ActorRef) extends Actor {
+trait LocalInterface[P <: Proposition, TX <: Transaction[P], PMOD <: PersistentNodeViewModifier[P, TX]]
+  extends Actor {
+
+  val viewHolderRef: ActorRef
 
   override def preStart(): Unit = {
     val events = Seq(
@@ -21,20 +26,32 @@ class LocalInterface(viewHolderRef: ActorRef) extends Actor {
   }
 
   private def viewHolderEvents: Receive = {
-    case StartingPersistentModifierApplication(pmod) =>
+    case StartingPersistentModifierApplication(pmod: PMOD) =>
+      onStartingPersistentModifierApplication(pmod)
 
-    case FailedTransaction(tx, throwable, source) =>
+    case FailedTransaction(tx: TX, throwable, source) =>
+      onFailedTransaction(tx)
+
+    case FailedModification(mod: PMOD, throwable, source) =>
+      onFailedModification(mod)
+
+    case SuccessfulTransaction(tx: TX, source) =>
+      onSuccessfulTransaction(tx)
 
 
-    case FailedModification(mod, throwable, source) =>
-
-
-    case SuccessfulTransaction(tx, source) =>
-
-
-    case SuccessfulModification(mod, source) =>
-
+    case SuccessfulModification(mod: PMOD, source) =>
+      onSuccessfulModification(mod)
   }
+
+  protected def onStartingPersistentModifierApplication(pmod: PMOD): Unit
+
+  protected def onFailedTransaction(tx: TX): Unit
+
+  protected def onFailedModification(mod: PMOD): Unit
+
+  protected def onSuccessfulTransaction(tx: TX): Unit
+
+  protected def onSuccessfulModification(mod: PMOD): Unit
 
 
   override def receive: Receive = viewHolderEvents
