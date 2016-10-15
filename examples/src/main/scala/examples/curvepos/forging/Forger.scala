c5fd2adbb005531c5181c72ef0c0aac46bcbac7e
kushti <alex.chepurnoy@iohk.io>
2016-10-16 01:18:32 +0300

    Forger WIP1

--- examples/src/main/scala/examples/curvepos/forging/Forger.scala
+++ examples/src/main/scala/examples/curvepos/forging/Forger.scala
@@ -1,12 +1,33 @@
 package examples.curvepos.forging
 
-import akka.actor.{Actor, ActorRef}
-import akka.actor.Actor.Receive
+import akka.actor.Actor
+
+import scala.concurrent.duration._
 
 class Forger extends Actor {
+  import Forger._
+
+  //set to true for initial generator
+  private var forging = false
+
+  val blockGenerationDelay = 500
+
   override def receive: Receive = {
-    case _ =>
+    case StartMining =>
+      forging = true
+      context.system.scheduler.scheduleOnce(500.microsecond)(self ! Forge)
+    case StopMining =>
+      forging = false
+
+    case Forge =>
+      //todo: block generation
+      context.system.scheduler.scheduleOnce(500.microsecond)(self ! Forge)
   }
 }
 
+object Forger {
+  case object StartMining
+  case object StopMining
 
+  case object Forge
+}
