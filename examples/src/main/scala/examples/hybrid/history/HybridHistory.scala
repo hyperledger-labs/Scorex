b627a29164a89374dd7789e3c5ffea81e88cc2a6
kushti <alex.chepurnoy@iohk.io>
2016-10-27 18:55:27 +0300

    example2 WIP2: hybridHistory WIP

--- examples/src/main/scala/examples/hybrid/history/HybridHistory.scala
+++ examples/src/main/scala/examples/hybrid/history/HybridHistory.scala
@@ -1,26 +1,57 @@
 package examples.hybrid.history
 
-import examples.hybrid.blocks.HybridPersistentNodeViewModifier
+
+import java.io.File
+
+import examples.hybrid.blocks.{HybridPersistentNodeViewModifier, PosBlock, PowBlock, PowBlockCompanion}
 import examples.hybrid.state.SimpleBoxTransaction
-import scorex.core.NodeViewComponentCompanion
+import io.iohk.iodb.{ByteArrayWrapper, LSMStore}
+import scorex.core.{NodeViewComponentCompanion, NodeViewModifier}
 import scorex.core.NodeViewModifier.{ModifierId, ModifierTypeId}
 import scorex.core.consensus.History
 import scorex.core.consensus.History.{BlockId, RollbackTo}
+import scorex.core.settings.Settings
 import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 
 import scala.util.Try
 
-class HybridHistory
-  extends History[PublicKey25519Proposition, SimpleBoxTransaction, HybridPersistentNodeViewModifier, HybridSyncInfo, HybridHistory]{
+class HybridHistory(settings: Settings)
+  extends History[PublicKey25519Proposition, SimpleBoxTransaction, HybridPersistentNodeViewModifier, HybridSyncInfo, HybridHistory] {
+
+  require(NodeViewModifier.ModifierIdSize == 32, "32 bytes ids assumed")
+
+  val dataDirOpt = settings.dataDirOpt.ensuring(_.isDefined, "data dir must be specified")
+  val dataDir = dataDirOpt.get
+
+  /*
+  val powDir = new File(dataDir + "/pow")
+  powDir.mkdirs()
+
+  val posDir = new File(dataDir + "/pos")
+  posDir.mkdirs()*/
+
+  val iFile = new File(s"$dataDir/blocksindexes")
+  val blockStorage = new LSMStore(iFile)
 
   /**
     * Is there's no history, even genesis block
     *
     * @return
     */
-  override def isEmpty: Boolean = ???
+  override def isEmpty: Boolean = blockStorage.lastVersion <= 0
 
-  override def blockById(blockId: BlockId): Option[HybridPersistentNodeViewModifier] = ???
+  override def blockById(blockId: BlockId): Option[HybridPersistentNodeViewModifier] = Try {
+    Option(blockStorage.get(ByteArrayWrapper(blockId))).flatMap { bw =>
+      val bytes = bw.data
+      val mtypeId = bytes.head
+      (mtypeId match {
+        case t: Byte if t == PowBlock.ModifierTypeId =>
+          PowBlockCompanion.parse(bytes.tail)
+        case t: Byte if t == PosBlock.ModifierTypeId =>
+          PowBlockCompanion.parse(bytes.tail)
+      }).toOption
+    }
+  }.getOrElse(None)
 
   override def append(block: HybridPersistentNodeViewModifier): Try[(HybridHistory, Option[RollbackTo[HybridPersistentNodeViewModifier]])] = ???
 
