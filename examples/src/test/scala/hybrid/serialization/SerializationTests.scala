09ca2ac93a53e877f8cd6a8a9f034781ef895559
catena <catena@protonmail.com>
2016-11-02 09:46:17 +0300

    All serialization tests in one specification

--- examples/src/test/scala/hybrid/serialization/SerializationTests.scala
+++ examples/src/test/scala/hybrid/serialization/SerializationTests.scala
@@ -1,15 +1,23 @@
 package hybrid.serialization
 
 import examples.hybrid.history.HybridSyncInfo
+import examples.hybrid.state.SimpleBoxTransaction
 import hybrid.HybridGenerators
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 import org.scalatest.{Matchers, PropSpec}
 
-class HybridSyncInfoTest extends PropSpec
+class SerializationTests extends PropSpec
 with PropertyChecks
 with GeneratorDrivenPropertyChecks
 with Matchers
 with HybridGenerators {
+
+  property("SimpleBoxTransaction serialization") {
+    forAll(simpleBoxTransactionGen) { b: SimpleBoxTransaction =>
+      val parsed = b.companion.parse(b.bytes).get
+      parsed.bytes shouldEqual b.bytes
+    }
+  }
 
   property("HybridSyncInfo serialization") {
     forAll(hybridSyncInfoGen) { b: HybridSyncInfo =>
