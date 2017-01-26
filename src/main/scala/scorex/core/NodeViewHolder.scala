4ec65429439b063428cb6255336716ce3fee9523
catena <catena@protonmail.com>
2017-01-26 17:30:21 +0300

    One more test idea

--- src/main/scala/scorex/core/NodeViewHolder.scala
+++ src/main/scala/scorex/core/NodeViewHolder.scala
@@ -127,6 +127,7 @@ trait NodeViewHolder[P <: Proposition, TX <: Transaction[P], PMOD <: PersistentN
             notifySubscribers(EventType.SuccessfulPersistentModifier, SuccessfulModification[P, TX, PMOD](pmod, source))
 
           case Failure(e) =>
+            //TODO rollback history (or at least notify)
             log.warn(s"Can`t apply persistent modifier (id: ${Base58.encode(pmod.id)}, contents: $pmod) to minimal state", e)
             System.exit(1)
             notifySubscribers(EventType.FailedPersistentModifier, FailedModification[P, TX, PMOD](pmod, e, source))
--- testkit/README.md
+++ testkit/README.md
@@ -12,7 +12,8 @@ TODO
 
 ### Coming test scenarios:
 - Block application and rollback leads to the same state
-- Block application and rollback leads to the same history
+- Block application and rollback leads to the same history (rollback is not defined for history yet)
 - NodeView apply block to both state and history or don't apply to any of them
 - Wallet contains secrets for all it's public propositions
 - Transactions added to memory pool are available by id
+- It's not possible to apply transaction to a state twice
