4948952788641dbe579fc93284b8e45aeb7a4106
catena <catena@protonmail.com>
2017-04-18 16:12:22 +0300

    Add number of proofs in serialized ticket

--- examples/src/main/scala/examples/tailchain/core/Ticket.scala
+++ examples/src/main/scala/examples/tailchain/core/Ticket.scala
@@ -15,6 +15,8 @@ case class Ticket(minerKey: Array[Byte], partialProofs: Seq[Array[Byte]]) extend
     "minerKey" -> Base58.encode(minerKey).asJson,
     "proofs" -> partialProofs.map(Base58.encode).asJson
   ).asJson
+
+  override def toString: String = json.noSpaces
 }
 
 object TicketSerializer extends Serializer[Ticket] {
@@ -26,24 +28,25 @@ object TicketSerializer extends Serializer[Ticket] {
       require(bytes.length == bytes.length.toShort)
       Shorts.toByteArray(bytes.length.toShort) ++ bytes
     }
-    obj.minerKey ++ proofsBytes.reduce(_ ++ _)
+    obj.minerKey ++ Shorts.toByteArray(obj.partialProofs.length.toShort) ++ proofsBytes.reduce(_ ++ _)
   }
 
 
   override def parseBytes(bytes: Array[Byte]): Try[Ticket] = Try {
     @tailrec
-    def parseProofs(index: Int, acc: Seq[Array[Byte]] = Seq.empty): Seq[Array[Byte]] = {
-      if (bytes.length > index) {
+    def parseProofs(index: Int, proofNum: Int, acc: Seq[Array[Byte]] = Seq.empty): Seq[Array[Byte]] = {
+      if (proofNum > 0) {
         val proofSize = Shorts.fromByteArray(bytes.slice(index, index + 2))
         val proof = bytes.slice(index + 2, index + 2 + proofSize)
-        parseProofs(index + 2 + proofSize, proof +: acc)
+        parseProofs(index + 2 + proofSize, proofNum - 1, proof +: acc)
       } else {
         acc
       }
     }
 
     val minerKey = bytes.slice(0, MinerKeySize)
-    val proofs = parseProofs(MinerKeySize).reverse
+    val proofNum = Shorts.fromByteArray(bytes.slice(MinerKeySize, MinerKeySize + 2))
+    val proofs = parseProofs(MinerKeySize + 2, proofNum).reverse
     Ticket(minerKey, proofs)
   }
 }
