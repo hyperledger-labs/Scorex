c627f34ebf014b3b1e6a712793fa80b9acd1b1c0
Alex Chepurnoy <Alex.Chepurnoy@iohk.io>
2017-04-19 17:20:42 +0300

    SpaceSavingsCalculator

--- examples/src/main/scala/examples/tailchain/simulation/SpaceSavingsCalculator.scala
+++ examples/src/main/scala/examples/tailchain/simulation/SpaceSavingsCalculator.scala
@@ -2,12 +2,43 @@ package examples.tailchain.simulation
 
 import java.io.File
 
+import examples.curvepos.transaction.PublicKey25519NoncedBox
+
 object SpaceSavingsCalculator extends App {
 
-  val file = new File("results.csv")
+  val eta = 100
+  val start = 1000
+  val finish = 10000
 
-  scala.io.Source.fromFile(file)
+  val outSize = PublicKey25519NoncedBox.BoxLength
 
-  file.close()
+  val file = new File("/home/kushti/garbage/results.csv")
 
+  val lines = scala.io.Source.fromFile(file).getLines().toIndexedSeq
+
+  println(lines.head)
+
+  val data = lines.tail.take(finish).map(_.split(","))
+
+  val blockSizes = data.map(_.apply(8)).map(_.toLong)
+
+  val headerSizes = data.map(_.apply(5)).map(_.toLong)
+
+  val currentUtxoSizes = data.map(_.apply(2)).map(_.toLong * outSize)
+
+  (start to finish).foreach{h =>
+    val fullChain = blockSizes.take(h).sum
+    val lightChain = headerSizes.take(h - eta).sum + blockSizes.slice(h - eta, h).sum
+
+    val spvchain = headerSizes.take(h).sum
+
+    val fullSet = fullChain + currentUtxoSizes(h - 1)
+
+    val lightSet = lightChain + currentUtxoSizes(h - 1)
+
+    val miningSet = lightSet + currentUtxoSizes(h - eta - 1)
+
+    println(s"height: $h, full: $fullSet, spv: $spvchain, light: $lightSet, mining: $miningSet, " +
+      s"f/s: ${fullSet/spvchain.toDouble}, f/l: ${fullSet / lightSet.toDouble}, f/m: ${fullSet / miningSet.toDouble}")
+  }
 }
