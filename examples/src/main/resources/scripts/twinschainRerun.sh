8f14edffa49afd4a9a6217c50774e05cf0dc5f06
catena <catena@protonmail.com>
2017-05-01 17:53:40 +0300

    exclude seed node from ips

--- examples/src/main/resources/scripts/twinschainRerun.sh
+++ examples/src/main/resources/scripts/twinschainRerun.sh
@@ -6,9 +6,9 @@ source "$my_dir/ips.sh"
 for ip in "${ips[@]}"
 do
    echo "ubuntu@$ip"
-   ssh ubuntu@$ip pkill -f twinsChain
-   ssh ubuntu@$ip rm -rf /tmp/l.log
-   ssh ubuntu@$ip nohup java -jar /home/ubuntu/data/twinsChain.jar /home/ubuntu/data/settings.json > /tmp/l.log 2>&1 &
+   ssh ubuntu@$ip "pkill -f twinsChain"
+   ssh ubuntu@$ip "rm -rf /tmp/l.log"
+   ssh ubuntu@$ip "nohup java -jar /home/ubuntu/data/twinsChain.jar /home/ubuntu/data/settings.json > /tmp/l.log 2>&1 &"
 done
 
 
