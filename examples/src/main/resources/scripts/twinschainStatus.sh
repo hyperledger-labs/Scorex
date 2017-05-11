fe4b7c5fcd3a71d41dde4ae6c634d4c0e916fad5
catena <catena@protonmail.com>
2017-05-11 14:51:11 +0300

    Show status of all nodes

--- examples/src/main/resources/scripts/twinschainStatus.sh
+++ examples/src/main/resources/scripts/twinschainStatus.sh
@@ -3,7 +3,7 @@
 my_dir="$(dirname "$0")"
 source "$my_dir/ips.sh"
 
-for ip in "${ips[@]}"
+for ip in "${allIps[@]}"
 do
    echo "$ip"
    curl -s -X GET --header 'Accept: application/json' 'http://'$ip':9085/debug/info' | grep height
