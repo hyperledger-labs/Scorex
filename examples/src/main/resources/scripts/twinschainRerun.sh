b04cb815b2a790f900dc1844aa49a4148eef9289
catena <catena@protonmail.com>
2017-05-15 16:51:03 +0300

    request node status after restart

--- examples/src/main/resources/scripts/twinschainRerun.sh
+++ examples/src/main/resources/scripts/twinschainRerun.sh
@@ -9,12 +9,14 @@ do
    ssh ubuntu@$ip "pkill -f twinsChain"
    ssh ubuntu@$ip "rm -f /tmp/l.log && rm -rf /home/ubuntu/data/data && rm -f /home/ubuntu/data/l.log"
 
-#   ssh ubuntu@$ip "sed -i '/\"RparamX10\"/c\  \"RparamX10\": 8,' /home/ubuntu/data/settings.json"
+#   ssh ubuntu@$ip "sed -i '/\"node1\"/c\  \"name\": \"genesisNode\",' /home/ubuntu/data/settings.json"
+   ssh ubuntu@$ip "sed -i '/\"RparamX10\"/c\  \"RparamX10\": 2,' /home/ubuntu/data/settings.json"
 #   ssh ubuntu@$ip "sed -i '/\"addedMaxDelay\"/c\    \"addedMaxDelay\": 0,' /home/ubuntu/data/settings.json"
 #   ssh ubuntu@$ip "sed -i '/\"targetBlockDelayMillis\"/c\    \"targetBlockDelayMillis\": 60000,' /home/ubuntu/data/settings.json"
 
    ssh ubuntu@$ip "nohup java -jar -XX:-UseGCOverheadLimit /home/ubuntu/data/twinsChain.jar /home/ubuntu/data/settings.json > /tmp/l.log 2>&1 &"
    sleep 30
+   curl -s -X GET --header 'Accept: application/json' 'http://'$ip':9085/debug/info' | grep height
 done
 
 
