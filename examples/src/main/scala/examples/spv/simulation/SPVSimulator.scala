677406d17618b82defd73b16bdb6024fbc3896fd
catena <catena@protonmail.com>
2017-06-15 12:43:39 +0300

    Calculate unique blocks in SPV proofs

--- examples/src/main/scala/examples/spv/simulation/SPVSimulator.scala
+++ examples/src/main/scala/examples/spv/simulation/SPVSimulator.scala
@@ -1,13 +1,13 @@
 package examples.spv.simulation
 
-import examples.spv.{Algos, KMZProofSerializer}
+import examples.spv.{Algos, Header, KMZProofSerializer}
 import scorex.core.transaction.state.PrivateKey25519Companion
 import scorex.core.utils.ScorexLogging
 import scorex.crypto.hash.Blake2b256
 
 object SPVSimulator extends App with ScorexLogging with SimulatorFuctions {
 
-  val Height = 5000
+  val Height = 500000
   val Difficulty = BigInt(1)
   val stateRoot = Blake2b256("")
   val minerKeys = PrivateKey25519Companion.generateKeys(stateRoot)
@@ -21,13 +21,16 @@ object SPVSimulator extends App with ScorexLogging with SimulatorFuctions {
 
   val k = 6
 
-  println("Chain constructed!")
+  println(s"Chain of length $Height, k=$k")
+  println("m,proofLength,blockNum,uniqueBlockNum")
 
   Seq(6, 15, 30, 50, 100, 127) foreach { m =>
     val proof = Algos.constructKMZProof(m, k, headerChain).get
     proof.valid.get
-    val blockNum = proof.suffix.length + proof.prefixProofs.flatten.length
-    println( m + " => " + KMZProofSerializer.toBytes(proof).length + "," + blockNum)
+    val blocks:Seq[Header] = proof.suffix ++ proof.prefixProofs.flatten
+    val blockNum = blocks.length
+    val uniqueBlockNum = blocks.map(_.encodedId).toSet.size
+    println( m + "," + KMZProofSerializer.toBytes(proof).length + "," + blockNum + "," + uniqueBlockNum)
   }
 
 
