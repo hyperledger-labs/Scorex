712d42d360439b6112a8d6a3b57a55b144409443
catena <catena@protonmail.com>
2017-06-15 16:35:11 +0300

    Serialization test method

--- testkit/src/main/scala/scorex/testkit/SerializationTests.scala
+++ testkit/src/main/scala/scorex/testkit/SerializationTests.scala
@@ -3,11 +3,13 @@ package scorex.testkit
 import org.scalacheck.Gen
 import org.scalatest.Matchers
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import scorex.core.serialization.Serializer
+import scorex.core.serialization.{BytesSerializable, Serializer}
 
 trait SerializationTests extends GeneratorDrivenPropertyChecks with Matchers {
-  def checkSerializationRoundtrip[A](generator: Gen[A], serializer: Serializer[A]) = {
+
+  def checkSerializationRoundtrip[A <: BytesSerializable](generator: Gen[A]): Unit = {
     forAll(generator) { b: A =>
+      val serializer: Serializer[A] = b.serializer.asInstanceOf[Serializer[A]]
       val recovered = serializer.parseBytes(serializer.toBytes(b)).get
       serializer.toBytes(b) shouldEqual serializer.toBytes(recovered)
     }
