f3865b6a271b7e095f719286c080a2222d3e0f35
Mayank <mayank.cse14@itbhu.ac.in>
2017-07-18 12:57:51 +0900

    Added new generators to support the new rollback property
    Added new generators to support the new rollback property - "State changes application and rollback leads to rollback of changes"

--- examples/src/test/scala/hybrid/HybridSanity.scala
+++ examples/src/test/scala/hybrid/HybridSanity.scala
@@ -104,6 +104,48 @@ class HybridSanity extends BlockchainSanity[PublicKey25519Proposition,
     }
   }.apply(Gen.Parameters.default, Seed.random()).get
 
+
+  override def genValidTransactionPair(curHistory: HybridHistory): Seq[SimpleBoxTransaction] = {
+    var keys = key25519Gen.apply(Gen.Parameters.default, Seed.random()).get
+    var value = positiveLongGen.apply(Gen.Parameters.default, Seed.random()).get
+
+
+    var newBox : IndexedSeq[(PublicKey25519Proposition, Long)] = IndexedSeq((keys._2, value))
+    var trx : SimpleBoxTransaction = simpleBoxTransactionGenCustomMakeBoxes(newBox).apply(Gen.Parameters.default, Seed.random()).get
+    var useBox: IndexedSeq[(PrivateKey25519, Long)] = IndexedSeq((keys._1, trx.newBoxes.toVector(0).nonce))
+
+    var trxnPair = Seq[SimpleBoxTransaction]()
+    trxnPair = trxnPair :+ trx
+    trxnPair = trxnPair :+ simpleBoxTransactionGenCustomUseBoxes(useBox).apply(Gen.Parameters.default, Seed.random()).get
+
+    trxnPair
+  }
+
+  override def genValidModifierCustomTransactions(curHistory: HybridHistory, trx: SimpleBoxTransaction): HybridBlock = {
+    if (curHistory.pairCompleted) {
+      for {
+        timestamp: Long <- positiveLongGen
+        nonce: Long <- positiveLongGen
+        brothersCount: Byte <- positiveByteGen
+        proposition: PublicKey25519Proposition <- propositionGen
+        brothers <- Gen.listOfN(brothersCount, powHeaderGen)
+      } yield {
+        val brotherBytes = PowBlockCompanion.brotherBytes(brothers)
+        val brothersHash: Array[Byte] = FastCryptographicHash(brotherBytes)
+        new PowBlock(curHistory.bestPowId, curHistory.bestPosId, timestamp, nonce, brothersCount, brothersHash, proposition, brothers)
+      }
+    } else {
+      for {
+        timestamp: Long <- positiveLongGen
+        txs: Seq[SimpleBoxTransaction] = Seq(trx)
+        box: PublicKey25519NoncedBox <- noncedBoxGen
+        attach: Array[Byte] <- genBoundedBytes(0, 4096)
+        generator: PrivateKey25519 <- key25519Gen.map(_._1)
+      } yield PosBlock.create(curHistory.bestPowId, timestamp, txs, box.copy(proposition = generator.publicImage), attach, generator)
+    }
+  }.apply(Gen.Parameters.default, Seed.random()).get
+
+
   def simpleMempoolTransactionGen (noOfTransactionsFromMempool : Int) : Seq[SimpleBoxTransaction] = {
     var randm = scala.util.Random
     var a = 0
