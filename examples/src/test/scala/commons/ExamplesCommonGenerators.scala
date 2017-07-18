2211feaa80e0b3ba069d0a1b88f96fc0b30e3f01
Mayank <mayank.cse14@itbhu.ac.in>
2017-07-18 13:00:22 +0900

    Added new functions to support the new rollback property
    Added new transaction generation functions to support the new rollback property - "State changes application and rollback leads to rollback of changes"

--- examples/src/test/scala/commons/ExamplesCommonGenerators.scala
+++ examples/src/test/scala/commons/ExamplesCommonGenerators.scala
@@ -24,5 +24,19 @@ trait ExamplesCommonGenerators extends CoreGenerators {
     to: IndexedSeq[(PublicKey25519Proposition, Long)] <- smallInt.flatMap(i => Gen.listOfN(i, pGen).map(_.toIndexedSeq))
   } yield SimpleBoxTransaction(from, to, fee, timestamp)
 
+  def simpleBoxTransactionGenCustomMakeBoxes (toBoxes: IndexedSeq[(PublicKey25519Proposition, Long)]): Gen[SimpleBoxTransaction] = for {
+    fee <- positiveLongGen
+    timestamp <- positiveLongGen
+    from: IndexedSeq[(PrivateKey25519, Long)] <- Gen.choose(1,1).flatMap(i => Gen.listOfN(i + 1, privGen).map(_.toIndexedSeq))
+    to = toBoxes
+  } yield SimpleBoxTransaction(from, to, fee, timestamp)
+
+  def simpleBoxTransactionGenCustomUseBoxes (fromBoxes: IndexedSeq[(PrivateKey25519, Long)]): Gen[SimpleBoxTransaction] = for {
+    fee <- positiveLongGen
+    timestamp <- positiveLongGen
+    from = fromBoxes
+    to: IndexedSeq[(PublicKey25519Proposition, Long)] <- Gen.choose(1,1).flatMap(i => Gen.listOfN(i, pGen).map(_.toIndexedSeq))
+  } yield SimpleBoxTransaction(from, to, fee, timestamp)
+
 
 }
