79abc0ffc7165f987e45ce9a22d9ffb6c862c78b
Aleksei <daron666@yandex.ru>
2017-08-28 14:09:02 +0400

    Added common way to create temp file.

---
+++ testkit/src/main/scala/scorex/testkit/utils/FileUtils.scala
@@ -0,0 +1,14 @@
+package scorex.testkit.utils
+
+trait FileUtils {
+
+  protected val randomPrefixLength = 10
+
+  def createTempFile: java.io.File = {
+    val rndString = scala.util.Random.alphanumeric.take(randomPrefixLength).mkString
+    val file = java.nio.file.Files.createTempDirectory(rndString).toFile
+    file.delete()
+    file
+  }
+
+}
