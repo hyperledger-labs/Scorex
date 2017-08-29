461173e11ae8eb280c00e2116274c49db1ddc4c3
Alex Chepurnoy <Alex.Chepurnoy@iohk.io>
2017-08-29 22:45:41 +0300

    deterministic tx boxes test fix

--- examples/src/test/scala/hybrid/state/SimpleBoxTransactionSpecification.scala
+++ examples/src/test/scala/hybrid/state/SimpleBoxTransactionSpecification.scala
@@ -1,6 +1,7 @@
 package hybrid.state
 
 import examples.commons.SimpleBoxTransaction
+import examples.curvepos.transaction.PublicKey25519NoncedBox
 import examples.hybrid.state.HBoxStoredState
 import hybrid.HybridGenerators
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
@@ -8,6 +9,7 @@ import org.scalatest.{Matchers, PropSpec}
 import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 import scorex.core.transaction.proof.Signature25519
 import scorex.core.transaction.state.PrivateKey25519Companion
+import scorex.crypto.encode.Base58
 import scorex.crypto.hash.Sha256
 
 class SimpleBoxTransactionSpecification extends PropSpec
@@ -26,8 +28,18 @@ class SimpleBoxTransactionSpecification extends PropSpec
 
     val genesisAccount = PrivateKey25519Companion.generateKeys("genesis".getBytes)._1
     val tx = SimpleBoxTransaction(IndexedSeq(genesisAccount -> 0), icoMembers.map(_ -> GenesisBalance), 0L, 0L)
-    tx.newBoxes.toString() shouldBe "Vector(PublicKey25519NoncedBox(3m6nhP4AZjFn5pgMd3PvH6PwHx23AG4tvpLCuu7Wt3hhAPssKc,-6219502975712200872,100000), PublicKey25519NoncedBox(4ZJwiEzpTHhvT6BMYZg1FUXysHkuBLRHb7FvXhZGx6HtsWZCeG,2326174055960855030,100000), PublicKey25519NoncedBox(3Y7Ji8wrYZ12EPup6ky2mWEaNo1wTgUKVPJ84xaHwHqTC6LXoh,-2090466149357841238,100000), PublicKey25519NoncedBox(3WqPcQ1w1HEaEDvHpnnqqYxJBzQGcf5gT5G5CrsXFL7UX4SA2N,-4786344880748433993,100000), PublicKey25519NoncedBox(4m5cG82kztD9bZVf1Tc1Ni1uvHobpKYuAUyxNSnDm7WLGCZvZh,2879476891976400353,100000), PublicKey25519NoncedBox(4huPANjYcqcdRm99tsCw29JqFnHMTJZsQjoufRQTEDPPoWmPSt,4610029492489107892,100000), PublicKey25519NoncedBox(3s3CauhVba81UefEuuaNqRqGLEV9jCZJpvLFg5dJdu29TivRZk,416797087985622128,100000), PublicKey25519NoncedBox(3HHuHxBf2eXmbUcGuFCx3dU6Wp7imeRiN5uz4rYDdQwsLwnwW4,-8485818448745401936,100000), PublicKey25519NoncedBox(38uZVfModMnCg5FSECtFiBE7Dbjmh7Tt1SgBD8gFTA1XDHxiqQ,-4750873086163930339,100000), PublicKey25519NoncedBox(3WTH7tB28nkbC9KFJTy8EBn1bWkxryiLKDnngeP9BYyuCik3aP,1904873933279744536,100000))"
-
+    tx.newBoxes.toSeq shouldBe
+      Vector(
+        PublicKey25519NoncedBox(PublicKey25519Proposition(Base58.decode("3m6nhP4AZjFn5pgMd3PvH6PwHx23AG4tvpLCuu7Wt3hhAPssKc").get.take(33).tail),-6219502975712200872L,100000),
+        PublicKey25519NoncedBox(PublicKey25519Proposition(Base58.decode("4ZJwiEzpTHhvT6BMYZg1FUXysHkuBLRHb7FvXhZGx6HtsWZCeG").get.take(33).tail),2326174055960855030L,100000),
+        PublicKey25519NoncedBox(PublicKey25519Proposition(Base58.decode("3Y7Ji8wrYZ12EPup6ky2mWEaNo1wTgUKVPJ84xaHwHqTC6LXoh").get.take(33).tail),-2090466149357841238L,100000),
+        PublicKey25519NoncedBox(PublicKey25519Proposition(Base58.decode("3WqPcQ1w1HEaEDvHpnnqqYxJBzQGcf5gT5G5CrsXFL7UX4SA2N").get.take(33).tail),-4786344880748433993L,100000),
+        PublicKey25519NoncedBox(PublicKey25519Proposition(Base58.decode("4m5cG82kztD9bZVf1Tc1Ni1uvHobpKYuAUyxNSnDm7WLGCZvZh").get.take(33).tail),2879476891976400353L,100000),
+        PublicKey25519NoncedBox(PublicKey25519Proposition(Base58.decode("4huPANjYcqcdRm99tsCw29JqFnHMTJZsQjoufRQTEDPPoWmPSt").get.take(33).tail),4610029492489107892L,100000),
+        PublicKey25519NoncedBox(PublicKey25519Proposition(Base58.decode("3s3CauhVba81UefEuuaNqRqGLEV9jCZJpvLFg5dJdu29TivRZk").get.take(33).tail),416797087985622128L,100000),
+        PublicKey25519NoncedBox(PublicKey25519Proposition(Base58.decode("3HHuHxBf2eXmbUcGuFCx3dU6Wp7imeRiN5uz4rYDdQwsLwnwW4").get.take(33).tail),-8485818448745401936L,100000),
+        PublicKey25519NoncedBox(PublicKey25519Proposition(Base58.decode("38uZVfModMnCg5FSECtFiBE7Dbjmh7Tt1SgBD8gFTA1XDHxiqQ").get.take(33).tail),-4750873086163930339L,100000),
+        PublicKey25519NoncedBox(PublicKey25519Proposition(Base58.decode("3WTH7tB28nkbC9KFJTy8EBn1bWkxryiLKDnngeP9BYyuCik3aP").get.take(33).tail),1904873933279744536L,100000))
   }
 
   property("Generated transaction is valid") {
