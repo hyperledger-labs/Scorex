4831d4385e0a51cce615cbb4755b074cce83aac8
catena <catena@protonmail.com>
2017-11-15 19:42:10 +0300

    Do not use Array[Byte] as a key in DeliveryTracker

--- src/main/scala/scorex/core/network/DeliveryTracker.scala
+++ src/main/scala/scorex/core/network/DeliveryTracker.scala
@@ -8,12 +8,15 @@ import scala.collection.mutable
 // in order to ban or de-prioritize peers that deliver what is not expected
 class DeliveryTracker {
 
+  protected type MapKey = scala.collection.mutable.WrappedArray.ofByte
+  protected def key(id: ModifierId): MapKey = new mutable.WrappedArray.ofByte(id)
+
   // when a remote peer is asked a modifier, we add the expected data to `expecting`
   // when a remote peer delivers expected data, it is removed from `expecting` and added to `delivered`.
   // when a remote peer delivers unexpected data, it is added to `deliveredSpam`.
   protected val expecting = mutable.Set[(ModifierTypeId, ModifierId, ConnectedPeer)]()
-  protected val delivered = mutable.Map[ModifierId, ConnectedPeer]()
-  protected val deliveredSpam = mutable.Map[ModifierId, ConnectedPeer]()
+  protected val delivered = mutable.Map[MapKey, ConnectedPeer]()
+  protected val deliveredSpam = mutable.Map[MapKey, ConnectedPeer]()
 
   def expect(cp: ConnectedPeer, mtid: ModifierTypeId, mids: Seq[ModifierId]): Unit = {
     for (id <- mids) expecting += ((mtid, id, cp))
@@ -25,19 +28,19 @@ class DeliveryTracker {
   def receive(mtid: ModifierTypeId, mid: ModifierId, cp: ConnectedPeer): Unit = {
     if (isExpecting(mtid, mid, cp)) {
       expecting -= ((mtid, mid, cp))
-      delivered(mid) = cp
+      delivered(key(mid)) = cp
     }
     else {
-      deliveredSpam(mid) = cp
+      deliveredSpam(key(mid)) = cp
     }
   }
 
-  def delete(mids: Seq[ModifierId]): Unit = for (id <- mids) delivered -= id
+  def delete(mids: Seq[ModifierId]): Unit = for (id <- mids) delivered -= key(id)
 
-  def deleteSpam(mids: Seq[ModifierId]): Unit = for (id <- mids) deliveredSpam -= id
+  def deleteSpam(mids: Seq[ModifierId]): Unit = for (id <- mids) deliveredSpam -= key(id)
 
-  def isSpam(mid: ModifierId): Boolean = deliveredSpam.exists(_._1 sameElements mid)
+  def isSpam(mid: ModifierId): Boolean = deliveredSpam contains key(mid)
 
-  def peerWhoDelivered(mid: ModifierId): Option[ConnectedPeer] = delivered.get(mid)
+  def peerWhoDelivered(mid: ModifierId): Option[ConnectedPeer] = delivered.get(key(mid))
 
 }
