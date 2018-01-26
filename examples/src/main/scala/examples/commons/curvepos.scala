bd69e1dad8a1bab3497d719191e780297d61bc48
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-01-26 18:39:06 +0300

    commons.scala cleaning

--- examples/src/main/scala/examples/commons/curvepos.scala
+++ examples/src/main/scala/examples/commons/curvepos.scala
@@ -4,20 +4,9 @@ import supertagged.TaggedType
 
 package object commons {
 
-  object GenerationSignature extends TaggedType[Array[Byte]]
-
-  object BaseTarget extends TaggedType[Long]
-
   object Value extends TaggedType[Long]
-
   object Nonce extends TaggedType[Long]
 
-  type GenerationSignature = GenerationSignature.Type
-
-  type BaseTarget = BaseTarget.Type
-
   type Value = Value.Type
-
   type Nonce = Nonce.Type
-
 }
