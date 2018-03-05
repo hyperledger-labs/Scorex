313a1e0ea229e31b90e95d12c23147d83ea9de64
Ilya Oskin <ilya.arcadich@gmail.com>
2018-03-05 18:53:36 +0300

    Redundant RollbackFailed object removed NodeViewLocalInterfaceSharedMessages.

--- src/main/scala/scorex/core/NodeViewLocalInterfaceSharedMessages.scala
+++ src/main/scala/scorex/core/NodeViewLocalInterfaceSharedMessages.scala
@@ -40,8 +40,6 @@ object NodeViewLocalInterfaceSharedMessages {
     //node view holder starting persistent modifier application
     case class StartingPersistentModifierApplication[PMOD <: PersistentNodeViewModifier](modifier: PMOD) extends NodeViewHolderEvent
     case class NewOpenSurface(newSurface: Seq[ModifierId]) extends NodeViewHolderEvent
-    //todo: consider sending info on the rollback
-    case object RollbackFailed extends NodeViewHolderEvent
   }
 }
 
