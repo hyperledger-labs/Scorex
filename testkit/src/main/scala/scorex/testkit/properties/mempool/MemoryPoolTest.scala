c75b1510e29f945d54082237eb41387c03079a59
Anatoly Shamov <anatoly.shamov@gmail.com>
2018-03-21 14:58:01 +0300

    mempool generator added (for testing stateful mempools)

--- testkit/src/main/scala/scorex/testkit/properties/mempool/MemoryPoolTest.scala
+++ testkit/src/main/scala/scorex/testkit/properties/mempool/MemoryPoolTest.scala
@@ -7,5 +7,6 @@ import scorex.core.transaction.{MemoryPool, Transaction}
 
 trait MemoryPoolTest[P <: Proposition, TX <: Transaction[P], MPool <: MemoryPool[TX, MPool]] {
   val memPool: MPool
+  val memPoolGenerator: Gen[MPool]
   val transactionGenerator: Gen[TX]
 }
