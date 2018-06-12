abf16a005639d8c819bc6cd3935f45efffd2302c
catena <catena@protonmail.com>
2018-06-12 22:09:40 +0300

    ico members of Hybryd are Base58 encoded

--- examples/src/main/scala/examples/hybrid/HybridNodeViewHolder.scala
+++ examples/src/main/scala/examples/hybrid/HybridNodeViewHolder.scala
@@ -14,6 +14,7 @@ import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 import scorex.core.transaction.state.PrivateKey25519Companion
 import scorex.core.utils.{NetworkTimeProvider, ScorexLogging}
 import scorex.core.{ModifierTypeId, NodeViewHolder, NodeViewModifier}
+import scorex.crypto.encode.Base58
 import scorex.crypto.signatures.PublicKey
 
 
@@ -122,7 +123,7 @@ object HybridNodeViewHolder extends ScorexLogging {
       "GXkCiK2P7khngAtfhG8TSqm4nfPbpMDNFBiG8CF41ZtP",
       "8etCeR343fg5gktxMh5j64zofFvWuyNTwmHAzWbsptoC",
       "AnwYrjV3yb9NuYWz31C758TZGTUCLD7zZdSYubbewygt"
-    ).map(s => PublicKey25519Proposition(PublicKey @@ encoder.decode(s).get))
+    ).map(s => PublicKey25519Proposition(PublicKey @@ Base58.decode(s).get))
       .ensuring(_.length == GenesisAccountsNum)
 
     val genesisAccount = PrivateKey25519Companion.generateKeys("genesis".getBytes)
