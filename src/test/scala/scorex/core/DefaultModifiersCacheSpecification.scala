5095eeed18b46ae0acfacbb6b6de2272a5df5f24
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-06-20 19:50:59 +0300

    put() and remove() test in DefaultModifiersCacheSpecification

--- src/test/scala/scorex/core/DefaultModifiersCacheSpecification.scala
+++ src/test/scala/scorex/core/DefaultModifiersCacheSpecification.scala
@@ -14,56 +14,89 @@ class DefaultModifiersCacheSpecification extends PropSpec
   with GeneratorDrivenPropertyChecks
   with Matchers {
 
+  private class FakeModifier extends PersistentNodeViewModifier {
+    override def parentId: ModifierId = ???
+    override val modifierTypeId: ModifierTypeId = ModifierTypeId @@ (0: Byte)
+    override def id: ModifierId = ???
+    override type M = this.type
+    override def serializer: Serializer[FakeModifier.this.type] = ???
+  }
+
+  private class FakeSyncInfo extends SyncInfo {
+    override def startingPoints: ModifierIds = ???
+    override type M = this.type
+    override def serializer: Serializer[FakeSyncInfo.this.type] = ???
+  }
+
+  private class FakeHr extends HistoryReader[FakeModifier, FakeSyncInfo] {
+    override def isEmpty: Boolean = ???
+    override def applicableTry(modifier: FakeModifier): Try[Unit] = ???
+    override def modifierById(modifierId: ModifierId): Option[FakeModifier] = ???
+    override def isSemanticallyValid(modifierId: ModifierId): ModifierSemanticValidity = ???
+    override def openSurfaceIds(): Seq[ModifierId] = ???
+    override def continuationIds(info: FakeSyncInfo, size: Int): Option[ModifierIds] = ???
+    override def syncInfo: FakeSyncInfo = ???
+    override def compare(other: FakeSyncInfo): History.HistoryComparisonResult = ???
+    override type NVCT = this.type
+  }
+
+  property("cache - put() and remove() basics") {
+    val limit = 3
+
+    val v1 = Blake2b256.hash("1")
+    val v2 = Blake2b256.hash("2")
+
+    val cache = new DefaultModifiersCache[FakeModifier, FakeHr](limit)
+
+    cache.maxSize shouldBe limit
+
+    cache.put(v1, new FakeModifier)
+    cache.contains(v1) shouldBe true
+    cache.size shouldBe 1
+    cache.put(v1, new FakeModifier)
+    cache.size shouldBe 1
+
+    cache.put(v2, new FakeModifier)
+    cache.size shouldBe 2
+    cache.contains(v2) shouldBe true
+    cache.remove(v2)
+    cache.size shouldBe 1
+    cache.contains(v2) shouldBe false
+
+    cache.put(v2, new FakeModifier)
+    cache.size shouldBe 2
+    cache.contains(v2) shouldBe true
+    cache.remove(v2, rememberKey = true)
+    cache.contains(v2) shouldBe true
+    cache.size shouldBe 1
+  }
+
   property("cache has limits") {
-    class FakeModifier extends PersistentNodeViewModifier {
-      override def parentId: ModifierId = ???
-      override val modifierTypeId: ModifierTypeId = ModifierTypeId @@ (0: Byte)
-      override def id: ModifierId = ???
-      override type M = this.type
-      override def serializer: Serializer[FakeModifier.this.type] = ???
-    }
-
-    class FakeSyncInfo extends SyncInfo {
-      override def startingPoints: ModifierIds = ???
-      override type M = this.type
-      override def serializer: Serializer[FakeSyncInfo.this.type] = ???
-    }
-
-    class FakeHr extends HistoryReader[FakeModifier, FakeSyncInfo] {
-      override def isEmpty: Boolean = ???
-      override def applicableTry(modifier: FakeModifier): Try[Unit] = ???
-      override def modifierById(modifierId: ModifierId): Option[FakeModifier] = ???
-      override def isSemanticallyValid(modifierId: ModifierId): ModifierSemanticValidity = ???
-      override def openSurfaceIds(): Seq[ModifierId] = ???
-      override def continuationIds(info: FakeSyncInfo, size: Int): Option[ModifierIds] = ???
-      override def syncInfo: FakeSyncInfo = ???
-      override def compare(other: FakeSyncInfo): History.HistoryComparisonResult = ???
-      override type NVCT = this.type
-    }
+    val limit = 3
 
     val v1 = Blake2b256.hash("1")
     val v2 = Blake2b256.hash("2")
     val v3 = Blake2b256.hash("3")
     val v4 = Blake2b256.hash("4")
 
-    val cache = new DefaultModifiersCache[FakeModifier, FakeHr](3)
+    val cache = new DefaultModifiersCache[FakeModifier, FakeHr](limit)
 
-    cache.maxSize shouldBe 3
+    cache.maxSize shouldBe limit
 
     cache.put(v1, new FakeModifier)
     cache.put(v2, new FakeModifier)
     cache.put(v3, new FakeModifier)
 
     cache.contains(v1) shouldBe true
-    cache.size shouldBe 3
+    cache.size shouldBe limit
 
     cache.put(v4, new FakeModifier)
     cache.contains(v1) shouldBe false
-    cache.size shouldBe 3
+    cache.size shouldBe limit
 
     cache.put(v1, new FakeModifier)
     cache.contains(v1) shouldBe true
-    cache.size shouldBe 3
+    cache.size shouldBe limit
     cache.contains(v2) shouldBe false
   }
 }
