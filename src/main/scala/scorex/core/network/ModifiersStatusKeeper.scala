dcd4a65e9d1d74eed2486c182d3a3425de67aa9a
catena <catena@protonmail.com>
2018-07-09 15:49:28 +0300

    Use guava cache in ModifiersStatusKeeper

--- src/main/scala/scorex/core/network/ModifiersStatusKeeper.scala
+++ src/main/scala/scorex/core/network/ModifiersStatusKeeper.scala
@@ -1,55 +1,68 @@
 package scorex.core.network
 
+import java.util.concurrent.TimeUnit
+
+import com.google.common.cache.CacheBuilder
 import scorex.core.ModifierId
 import scorex.core.consensus.History
 import scorex.core.network.ModifiersStatusKeeper._
+import scorex.crypto.encode.Base16
 
-import scala.collection.concurrent.TrieMap
 import scala.collection.mutable
+import scalacache._
+import scalacache.guava._
+import scalacache.modes.sync._
 
 /**
+  *
   * Class that keeps modifiers statuses when modifier is known, but is not applied yet.
+  *
+  * @param maxSize     - maximum size cache to keep modifier statuseodis.
+  * @param maxLivetime - maximum livetime to keep modifier statuses.
   */
-class ModifiersStatusKeeper() {
+class ModifiersStatusKeeper(maxSize: Long = 100000L,
+                            maxLivetime: Long = 10L) {
 
   type K = mutable.WrappedArray[Byte]
 
-  // TODO some LRU cache might be useful here to limit this size and remove outdated statuses
-  private val statuses: TrieMap[K, ModifiersStatus] = TrieMap[K, ModifiersStatus]()
+  private val underlyingGuavaCache = CacheBuilder.newBuilder()
+    .maximumSize(maxSize)
+    .expireAfterWrite(maxLivetime, TimeUnit.MINUTES)
+    .build[String, Entry[ModifiersStatus]]
+  implicit val statuses: Cache[ModifiersStatus] = GuavaCache(underlyingGuavaCache)
 
   /**
     * @return status of modifier `id`.
     *         Since we do not keep statuses for already applied modifiers, `history` is required here.
     */
   def status(id: ModifierId)(history: History[_, _, _]): ModifiersStatus = {
-    val mKey = key(id)
-    statuses.getOrElse(mKey,
+    get(key(id)).getOrElse {
       if (history.contains(id)) {
         Applied
       } else {
         Unknown
       }
-    )
+    }
   }
 
-  def set(id: ModifierId, status: ModifiersStatus): Option[ModifiersStatus] = statuses.put(key(id), status)
+  def set(id: ModifierId, status: ModifiersStatus): Unit = put(key(id))(status)
 
   /**
     * We do not keep applied modifiers status since we can get their status from history
     */
-  def applied(id: ModifierId): Option[ModifiersStatus] = statuses.remove(key(id))
+  def applied(id: ModifierId): Unit = remove(key(id))
 
   /**
     * Modifier `id` was received from other peer
     */
-  def received(id: ModifierId): Option[ModifiersStatus] = set(id, Received)
+  def received(id: ModifierId): Unit = set(id, Received)
 
   /**
     * Modifier `id` was requested from other peer
     */
-  def requested(id: ModifierId): Option[ModifiersStatus] = set(id, Requested)
+  def requested(id: ModifierId): Unit = set(id, Requested)
 
-  protected def key(id: ModifierId) = new mutable.WrappedArray.ofByte(id)
+  protected def key(id: ModifierId): String = Base16.encode(id)
 }
 
 
