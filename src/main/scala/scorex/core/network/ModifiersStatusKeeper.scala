6f1e2b8adb4ca523ab767408b20e1fd8662a78e9
catena <catena@protonmail.com>
2018-07-27 16:51:58 +0300

    Log modifier status changes

--- src/main/scala/scorex/core/network/ModifiersStatusKeeper.scala
+++ src/main/scala/scorex/core/network/ModifiersStatusKeeper.scala
@@ -1,16 +1,16 @@
 package scorex.core.network
 
-import scorex.core.{ModifierId, NodeViewModifier}
-import scorex.core.consensus.{HistoryReader, ModifierContaining}
+import scorex.core.consensus.ModifierContaining
 import scorex.core.network.ModifiersStatus.{Applied, Invalid, ModifiersStatus, Received, Requested, Unknown}
+import scorex.core.utils.{ScorexEncoding, ScorexLogging}
+import scorex.core.{ModifierId, NodeViewModifier}
 
 import scala.collection.concurrent.TrieMap
-import scala.collection.mutable
 
 /**
   * Trait that keeps modifiers statuses when modifier is known, but is not applied yet.
   */
-trait ModifiersStatusKeeper {
+trait ModifiersStatusKeeper extends ScorexLogging with ScorexEncoding {
 
   private val statuses: TrieMap[ModifierId, ModifiersStatus] = TrieMap[ModifierId, ModifiersStatus]()
 
@@ -30,12 +30,19 @@ trait ModifiersStatusKeeper {
 
   def status(id: ModifierId, mk: ModifierContaining[_ <: NodeViewModifier]): ModifiersStatus = status(id, Seq(mk))
 
-  def set(id: ModifierId, status: ModifiersStatus): Option[ModifiersStatus] = statuses.put(id, status)
+  def set(id: ModifierId, status: ModifiersStatus): Option[ModifiersStatus] = {
+    log.trace(s"Set modifier ${encoder.encode(id)} to status $status")
+    if (status == Unknown || status == Applied) {
+      statuses.remove(id)
+    } else {
+      statuses.put(id, status)
+    }
+  }
 
   /**
     * Stop tracking this modifier
     */
-  def toApplied(id: ModifierId): Option[ModifiersStatus] = statuses.remove(id)
+  def toApplied(id: ModifierId): Option[ModifiersStatus] = set(id, Applied)
 
   /**
     * Modifier `id` was received from other peer
@@ -51,8 +58,11 @@ trait ModifiersStatusKeeper {
     * Remove status for modifier `id` for ModifiersStatusKeeper.
     * This may happen when received modifier bytes does not correspond to declared modifier id
     */
-  def toUnknown(id: ModifierId): Option[ModifiersStatus] = statuses.remove(id)
+  def toUnknown(id: ModifierId): Option[ModifiersStatus] = set(id, Unknown)
 
+  /**
+    * This modifier is permanently invalid - our not should not try to download and apply it
+    */
   def toInvalid(id: ModifierId): Option[ModifiersStatus] = set(id, Invalid)
 
 }
