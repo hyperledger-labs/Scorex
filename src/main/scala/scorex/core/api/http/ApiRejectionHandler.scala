ca59045f8322a6250069afc90790c5bfc2fc6f2d
oskin1 <ilya.arcadich@gmail.com>
2018-10-10 17:30:25 +0300

    Implicit .apply() method of ApiError is used.

--- src/main/scala/scorex/core/api/http/ApiRejectionHandler.scala
+++ src/main/scala/scorex/core/api/http/ApiRejectionHandler.scala
@@ -7,30 +7,30 @@ object ApiRejectionHandler {
   implicit val rejectionHandler: RejectionHandler = RejectionHandler.newBuilder()
     .handleAll[SchemeRejection] { rejections =>
       val schemes = rejections.map(_.supported).mkString(", ")
-      ApiError.BadRequest.complete(s"Uri scheme not allowed, supported schemes: $schemes")
+      ApiError.BadRequest(s"Uri scheme not allowed, supported schemes: $schemes")
     }
     .handle {
       case AuthorizationFailedRejection =>
-        ApiError.Forbidden.complete("The supplied authentication is not authorized to access this resource")
+        ApiError.Forbidden("The supplied authentication is not authorized to access this resource")
     }
     .handle {
       case MalformedRequestContentRejection(msg, _) =>
-        ApiError.BadRequest.complete("The request content was malformed:\n" + msg)
+        ApiError.BadRequest("The request content was malformed:\n" + msg)
     }
     .handle {
       case InvalidOriginRejection(allowedOrigins) =>
-        ApiError.Forbidden.complete(s"Allowed `Origin` header values: ${allowedOrigins.mkString(", ")}")
+        ApiError.Forbidden(s"Allowed `Origin` header values: ${allowedOrigins.mkString(", ")}")
     }
     .handle {
       case MissingQueryParamRejection(paramName) =>
-        ApiError.NotExists.complete(s"Request is missing required query parameter '$paramName'")
+        ApiError.NotExists(s"Request is missing required query parameter '$paramName'")
     }
     .handle {
       case RequestEntityExpectedRejection =>
-        ApiError.BadRequest.complete("Request entity expected but not supplied")
+        ApiError.BadRequest("Request entity expected but not supplied")
     }
-    .handle { case ValidationRejection(msg, _) => ApiError.BadRequest.complete(msg) }
-    .handle { case x => ApiError.InternalError.complete(s"Unhandled rejection: $x") }
-    .handleNotFound { ApiError.NotExists.complete("The requested resource could not be found.") }
+    .handle { case ValidationRejection(msg, _) => ApiError.BadRequest(msg) }
+    .handle { case x => ApiError.InternalError(s"Unhandled rejection: $x") }
+    .handleNotFound { ApiError.NotExists("The requested resource could not be found.") }
     .result()
 }
