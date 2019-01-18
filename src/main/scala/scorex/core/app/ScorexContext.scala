396084b03031e1a18b064b205862ab1607ee3440
vmikheev <victor.mikheev@gmail.com>
2019-01-18 13:01:00 +0300

    Revert ScorexContext changes

--- src/main/scala/scorex/core/app/ScorexContext.scala
+++ src/main/scala/scorex/core/app/ScorexContext.scala
@@ -2,7 +2,6 @@ package scorex.core.app
 
 import java.net.InetSocketAddress
 
-import scorex.core.network.message.Message.MessageCode
 import scorex.core.network.{PeerFeature, UPnPGateway}
 import scorex.core.network.message.MessageSpec
 import scorex.core.utils.TimeProvider
@@ -11,9 +10,4 @@ case class ScorexContext(messageSpecs: Seq[MessageSpec[_]],
                          features: Seq[PeerFeature],
                          upnpGateway: Option[UPnPGateway],
                          timeProvider: TimeProvider,
-                         externalNodeAddress: Option[InetSocketAddress]) {
-
-  import scala.language.existentials
-  val specsMap: Map[MessageCode, MessageSpec[_]] = Map(messageSpecs.map(s => s.messageCode -> s): _*)
-  require(specsMap.size == messageSpecs.size, "Duplicate message codes")
-}
+                         externalNodeAddress: Option[InetSocketAddress])
