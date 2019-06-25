6331984b24281703898bb003fcd70b7fc42351db
oskin1 <ilya.arcadich@gmail.com>
2019-06-25 12:14:50 +0300

    Correct requirement.

--- src/main/scala/scorex/core/consensus/History.scala
+++ src/main/scala/scorex/core/consensus/History.scala
@@ -85,8 +85,8 @@ object History {
                                                             toDownload: Seq[(ModifierTypeId, ModifierId)])
                                                            (implicit encoder: ScorexEncoder) {
 
-    require(branchPoint.isDefined == toRemove.nonEmpty, s"Branch point should be defined for non-empty toRemove," +
-      s" ${branchPoint.isDefined} == ${toRemove.nonEmpty} given")
+    if (toRemove.nonEmpty)
+      require(branchPoint.isDefined, s"Branch point should be defined for non-empty `toRemove`")
 
     lazy val chainSwitchingNeeded: Boolean = toRemove.nonEmpty
 
